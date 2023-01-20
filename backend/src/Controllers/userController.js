9937341169e3ec3d08d29f1bbd8eabd4de5edc35
Rupam Gupta <guptarupam.1020@gmail.com>
2023-01-21 01:01:05 +0530

    ErrorHandler Done

--- backend/src/Controllers/userController.js
+++ backend/src/Controllers/userController.js
@@ -1,77 +1,226 @@
-//______________________ Import or Require Modules ________________________________
+const catchAsyncError = require("../middlewares/catchAsyncError");
+const userModel = require("../models/userModel");
+const ErrorHandler = require("../utils/errorHandler");
+const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
-const authorModel = require("../Models/authorModel");
-const validator = require("../Validator/Validation");
-const jwt = require("jsonwebtoken");
-const catchAsyncError = require("../Middleware/catchAsyncError");
+//register User
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-//______________________ post api : Create Author ________________________________
+  const user = await userModel.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "myCloud.public_id",
+      url: "myCloud.secure_url",
+    },
+  });
+  sendToken(user, 200, res);
+});
 
-exports.createAuthor = catchAsyncError(async function (req, res) {
-  const data = req.body;
-  const { title, email, password } = data;
+// Login User
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
 
-  if (!title) {
-    return res.status(400).send({ status: false, msg: "title is required" });
-  } else {
-    if (title != "Mr" || title != "Mrs" || title != "Miss") {
-      return res
-        .status(400)
-        .send({ status: false, msg: "title can be Mr. Miss or Mrs " });
-    }
+  // checking if user has given password and email both
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Email & Password", 400));
   }
 
-  if (!validator.isValidEmail(email)) {
-    return res
-      .status(400)
-      .send({ status: false, msg: "Please Enter Valid Email Address" });
-  }
-  const isEmailAlreadyUsed = await authorModel.findOne({ email });
-  if (isEmailAlreadyUsed) {
-    return res.status(400).send({
-      status: false,
-      msg: "Oooh...Email already Registered. Please Login...",
-    });
+  const user = await userModel.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
   }
 
-  if (!validator.isValidPassword(password)) {
-    return res.status(400).send({
-      status: false,
-      msg: "Password is required and Should Contain Min 8 character and 1 Special Symbol",
-    });
-  }
-  const newAuthor = await authorModel.create(data);
+  const isPasswordMatched = await user.comparePassword(password);
 
-  res.status(201).send({
-    status: true,
-    msg: "Author Created successfully....",
-    data: newAuthor,
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+  sendToken(user, 200, res);
+});
+
+//logout
+exports.logout = catchAsyncError(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(new Date().getTime()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    sucess: true,
+    message: "Logged Out Successfully",
   });
 });
 
-//______________________ post api : Login Author ________________________________
+exports.forgotPassword = catchAsyncError(async (req, res, next) => {
+  const user = await userModel.findOne({ email: req.body.email });
 
-exports.logInUser = catchAsyncError(async function (req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-
-  if (!validator.isValidEmail(email)) {
-    return res.status(400).send({ status: false, msg: "Email is required" });
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
   }
 
-  if (!validator.isValidPassword(password)) {
-    return res.status(400).send({ status: false, msg: "password is required" });
+  // Get ResetPassword Token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  const resetPasswordUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Ecommerce Password Recovery`,
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
+// Reset Password
+exports.resetPassword = catchAsyncError(async (req, res, next) => {
+  // creating token hash
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await userModel.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(
+      new ErrorHandler(
+        "Reset Password Token is invalid or has been expired",
+        400
+      )
+    );
   }
 
-  const author = await authorModel.findOne({ email, password });
-  if (!author) {
-    return res
-      .status(401)
-      .send({ status: false, msg: "Invalid login credentials" });
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password does not password", 400));
   }
-  const token = jwt.sign(
-    { authorId: author._id.toString() },
-    process.env.SECRET_KEY
-  );
-  return res.status(200).send({ status: true, data: { token } });
+
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendToken(user, 200, res);
+});
+
+// Get User Detail
+exports.getUserDetails = catchAsyncError(async (req, res, next) => {
+  const user = await userModel.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// update User password
+exports.updatePassword = catchAsyncError(async (req, res, next) => {
+  const user = await userModel.findById(req.user.id).select("+password");
+
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Old password is incorrect", 400));
+  }
+
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return next(new ErrorHandler("password does not match", 400));
+  }
+
+  user.password = req.body.newPassword;
+
+  await user.save();
+
+  sendToken(user, 200, res);
+});
+
+
+
+// Get all users(admin)
+exports.getAllUser = catchAsyncError(async (req, res, next) => {
+  const users = await userModel.find();
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+// Get single user (admin)
+exports.getSingleUser = catchAsyncError(async (req, res, next) => {
+  const user = await userModel.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exist with Id: ${req.params.id}`)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// update User Role -- Admin
+exports.updateUserRole = catchAsyncError(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+
+  await userModel.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Delete User --Admin
+exports.deleteUser = catchAsyncError(async (req, res, next) => {
+  const user = await userModel.findByIdAndDelete(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exist with Id: ${req.params.id}`, 400)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "User Deleted Successfully",
+  });
 });
--- backend/src/Models/authorModel.js
+++ backend/src/Models/authorModel.js
@@ -1,39 +1,85 @@
-//______________________ Import or Require Modules ________________________________
-
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
-//____________________________ Creating Schema _____________________________________
-
-const authorSchema = new mongoose.Schema(
-  {
-    fname: {
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter Your Name"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+    minLength: [4, "Name should have more than 4 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please Enter Your Email"],
+    unique: true,
+    validate: [validator.isEmail, "Please Enter a valid Email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please Enter Your Password"],
+    minLength: [8, "Password should be greater than 8 characters"],
+    select: false,
+  },
+  avatar: {
+    public_id: {
       type: String,
-      required: [true, "Please Provide a First Name"],
-      trim: true,
+      required: true,
     },
-    lname: {
+    url: {
       type: String,
-      required: [true, "Please Provide a Last Name"],
-      trim: true,
-    },
-    title: {
-      type: String,
-      required: [true, "Please Provide a Title"],
-      enum: ["Mr", "Mrs", "Miss"],
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: [true, "Please Provide a Email"],
-    },
-    password: {
-      type: String,
-      required: [true, "Please Provide a Password"],
+      required: true,
     },
   },
-  { timestamps: true }
-);
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 
-//__________________________ Exporting Author Schema ___________________________________________
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
 
-module.exports = new mongoose.model("Author", authorSchema);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// JWT TOKEN
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// Compare Password
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Generating Password Reset Token
+userSchema.methods.getResetPasswordToken = function () {
+  // Generating Token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hashing and adding resetPasswordToken to userSchema
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+  return resetToken;
+};
+
+module.exports = mongoose.model("User", userSchema);
